0fdb3127a8ba330a75814748ce793062c079c5e6
Borguuh <abdrahmanadewale@gmail.com>
2021-09-12 12:44:19 +0100

    handling post not found error

--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -11,6 +11,7 @@ const DUMMY_POST = [
 		createdAt: '2021-08-18T03:22:56.637Z',
 		updatedAt: '2021-08-18T03:23:56.637Z',
 		author: {
+			uid: 'u1',
 			username: "Borguuh",
 			img: 'https://i.stack.imgur.com/xHWG8.jpg'
 		}
@@ -22,14 +23,26 @@ router.get('/:pid', (req, res, next) => {
   const post = DUMMY_POST.find(p => {
   	return p.id === postId;
   });
+  if (!post) {
+    const error = new Error('Could not find a post for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
   res.json({post});
 });
 
 router.get('/users/:uid', (req, res, next) => {
 	const userPost = req.params.uid;
 	const post = DUMMY_POST.find(p => {
-		return p.author.username === userPost;
+		return p.author.uid === userPost;
 	})
+  if (!post) {
+    const error = new Error('Could not find a post for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
+
 	res.json({post})
 }) 
 
