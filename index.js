52511e6f7c09211c09dc493640a8cafa8eba43ba
Borguuh <abdrahmanadewale@gmail.com>
2021-09-12 11:39:43 +0100

    setup post route

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const postsRoutes = require('./routes/posts-routes');
+
 const app = express();
+
+app.use(postsRoutes);
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
